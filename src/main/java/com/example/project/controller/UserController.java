15b25604e2dab994ef401c8847fc7928d10be822
Pigassi <ppigassi@gmail.com>
2022-02-01 16:17:11 -0300

    Second Commit

--- src/main/java/com/example/project/controller/UserController.java
+++ src/main/java/com/example/project/controller/UserController.java
@@ -4,23 +4,25 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.project.model.User;
 import com.example.project.repository.UserRepository;
 
-
-@Controller
-@RequestMapping("/")
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
+@RestController
 public class UserController {
 
 	@Autowired
 	private UserRepository userRepository;
 	
-	@GetMapping("/listUsers")
-	public List getUsers(){
-		return (List) userRepository.findAll(); 
-	}
+    @RequestMapping(value = "/userList", method = RequestMethod.GET)
+    public List<User> Get() {
+        return userRepository.findAll();
+    }
 }
 
